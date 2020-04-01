2c8054bcec47fe091983a938f0f0df03e8a42e99
Timrin <19280360+Timrin@users.noreply.github.com>
2020-04-01 15:20:30 +0200

    Initial commit
    Commit includes the default project files for a flutter project from IntelliJ/Android Studio

---
+++ android/app/src/main/kotlin/se/mau/dare20/dare_n_share_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package se.mau.dare20.dare_n_share_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
