d5acc899ca7d45210f70d13aebca3bde53d81994
Timrin <19280360+Timrin@users.noreply.github.com>
2020-05-01 00:18:27 +0200

    Implement classes to aid users in an error state

---
+++ lib/error_handling/error_feedback.dart
@@ -0,0 +1,21 @@
+import 'package:dare_n_share_app/error_handling/error_types.dart';
+import 'package:flutter/material.dart';
+
+class ErrorFeedback {
+  ErrorFeedback._(); //Private constructor
+
+     static const errorData = <ErrorTypes, dynamic>{
+       ErrorTypes.no_connection: {
+         "icon":Icons.cloud_off,
+         "message": "Could not reach the server",
+       },
+       ErrorTypes.user_has_no_dares: {
+         "icon":Icons.sentiment_dissatisfied,
+         "message": "You don't have any dares yet\nPress the + to start one!",
+       },
+       ErrorTypes.user_has_no_friends: {
+         "icon":Icons.group_add,
+         "message": "It's more fun with friends!\nLet's add some!",
+       },
+     };
+}
---
+++ lib/error_handling/error_types.dart
@@ -0,0 +1,5 @@
+enum ErrorTypes {
+  no_connection,
+  user_has_no_dares,
+  user_has_no_friends,
+}
