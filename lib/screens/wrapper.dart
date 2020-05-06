953df11c007a66d968f4e65cba453f4929c9081a
Timrin <19280360+Timrin@users.noreply.github.com>
2020-05-06 15:03:34 +0200

    Update the Authentication screen and make it the starting screen
    Update the look of the authentication screen and link the buttons to
    the correct screens
    Make the authentication screen the startig screen for logged out users

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,14 +1,14 @@
 import 'package:dare_n_share_app/controllers/controller.dart';
+import 'package:dare_n_share_app/screens/authenticate.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'home.dart';
-import 'login_user.dart';
 
 class Wrapper extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Provider.of<Controller>(context).userId == null ? LoginUser():Home();
+    return Provider.of<Controller>(context).userId == null ? Authenticate():Home();
   }
 
 }
