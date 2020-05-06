03670915b710d481af31c1bbe181a5752e5dd46d
Karolina Hammar <karolinahammar@Karolinas-MBP.lan>
2020-05-06 15:15:38 +0200

    Login

--- lib/screens/login_user.dart
+++ lib/screens/login_user.dart
@@ -4,7 +4,7 @@ import 'package:dare_n_share_app/screens/home.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-///@author Karolina Hammar
+///@author Karolina Hammar & Timothy Timrin
 ///Class to login existing user
 
 //todo add logic to check username and password
@@ -15,9 +15,14 @@ class LoginUser extends StatefulWidget {
 }
 
 class _LoginUserState extends State<LoginUser> {
+  bool _valid;
 
   String _email;
   String _password;
+
+  Pattern _pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|'
+      r'(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(('
+      r'[a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
 
   @override
   Widget build(BuildContext context) {
@@ -32,43 +37,59 @@ class _LoginUserState extends State<LoginUser> {
             bottom: TabBar(
               tabs: [
                 Tab(
-                  icon: Icon(Icons.home
-                    , color: ColorDesign.colorProfile,
+                  icon: Icon(
+                    Icons.home,
+                    color: ColorDesign.colorProfile,
                   ),
                   text: "home",
                 ),
                 Tab(
-                  icon: Icon(Icons.face,
-                    color: ColorDesign.colorProfile,),
+                  icon: Icon(
+                    Icons.face,
+                    color: ColorDesign.colorProfile,
+                  ),
                   text: "profile",
                 ),
               ],
             ),
-          )
-          ,
+          ),
           body: ListView(
             children: <Widget>[
-              SizedBox(height: 50,),
-              Center(
-                child: Text("Login",
-                  style: TextStyle(fontSize: 30),),
+              SizedBox(
+                height: 50,
               ),
-              SizedBox(height: 50,),
+              Center(
+                child: Text(
+                  "Login",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
               Text("Enter username"),
-              SizedBox(height: 10,),
-              enterExistingUsername(),
-              SizedBox(height: 30,),
+              SizedBox(
+                height: 10,
+              ),
+              enterExistingEmail(),
+              SizedBox(
+                height: 30,
+              ),
               Text("Enter password"),
-              SizedBox(height: 10,),
+              SizedBox(
+                height: 10,
+              ),
               enterExistingPassword(),
-              SizedBox(height: 50,),
+              SizedBox(
+                height: 50,
+              ),
               buttonLogin(context)
             ],
           ),
         ));
   }
 
-  Widget enterExistingUsername() {
+  Widget enterExistingEmail() {
     return TextFormField(
       onChanged: (value) {
         setState(() {
@@ -99,19 +120,71 @@ class _LoginUserState extends State<LoginUser> {
             child: Text("Login"),
           ),
         ),
-
-        onTap: (
-            ) {
+        onTap: () {
           print("$_email $_password");
 
           //TODO: Error handling
           AuthLogic().handleSignIn(_email, _password);
-
-          /*Navigator.push(context,
-              MaterialPageRoute(
-                builder: (context) => Home()));*/
+          _validate();
+         if (_valid == true) {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Home()));
+          }
         },
       ),
     );
+  }
+
+  ///Method to validate if entered password or email are correct
+
+  void _validate() {
+    bool isValid;
+    if (_password.length < 8) {
+      isValid = false;
+      _errorMessageDialog();
+    } else if (_validateEmail(_email) == false) {
+      isValid = false;
+      _errorMessageDialog();
+    } else{
+      isValid = true;
+    }
+
+    if(isValid == true) {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => Home()));
+    }
+    this._valid = isValid;
+    //return isValid;
+  }
+
+  ///Method to validate use of characters in email
+
+  bool _validateEmail(String email) {
+    RegExp reg = new RegExp(_pattern);
+    return (!reg.hasMatch(email)) ? false : true;
+  }
+
+  ///Method to display message dialog if entered email or password is incorrect
+
+  Future<void> _errorMessageDialog() async {
+    return showDialog<void>(
+        context: context,
+        barrierDismissible: false,
+
+        ///User must tap button to dismiss message
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("You´ve entered a wrong password or email, "
+                "\n  Password must be more than 7 characters"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Ok"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
   }
 }
