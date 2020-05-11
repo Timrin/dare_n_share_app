8e3da1424dbbd0ffff1c294bc59db279a1e1fd11
Timrin <19280360+Timrin@users.noreply.github.com>
2020-05-11 21:29:42 +0200

    Comment code in several classes

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,8 @@
+///Config contains static configuration information such as IP
+///
+///
 class Config {
   static const String IP = "68.183.78.52:2375";
-  static const String T_IP ="localhost:8080";//Test IP. This is for running tests outside of the emulator
-  static const String E_IP = "10.0.2.2:8080"; //Emulator IP, 10.0.2.2 points to localhost from the emulator
+  static const String TEST_IP ="localhost:8080";//Test IP. This is for running tests outside of the emulator
+  static const String EMULATOR_IP = "10.0.2.2:8080"; //Emulator IP, 10.0.2.2 points to localhost from the emulator
 }
--- lib/models/participant.dart
+++ lib/models/participant.dart
@@ -1,9 +1,12 @@
 import 'package:dare_n_share_app/models/user.dart';
 
+///Entity class for participants
+///A participant belongs to a Dare, and holds user information relating to that dare
 class Participant {
   final User user;
   final List score;
 
+  //Constructor
   Participant({this.user, this.score});
 
   @override
