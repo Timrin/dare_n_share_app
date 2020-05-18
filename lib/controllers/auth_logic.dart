22e076261eaaad1518bcf5aa4dd5bfc0c73efbc0
Timrin <19280360+Timrin@users.noreply.github.com>
2020-05-18 23:42:42 +0200

    Fix the user being signed in and out in the gui after registration fails
    Fixed a bug where the gui would update to a signed in state and then
    back to the starting screen. This happened when a user registration
    failed as a result of the dare server was down.
    Now the user stays on the registration page and receives an error
    message instead.

--- lib/controllers/auth_logic.dart
+++ lib/controllers/auth_logic.dart
@@ -1,6 +1,7 @@
 import 'package:dare_n_share_app/models/user.dart';
 import 'package:dare_n_share_app/services/auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 
 ///@author Timothy Timrin
 
@@ -35,14 +36,19 @@ class AuthLogic {
   ///
   /// Attempts to register a new user with the provided parameters
   /// The user is registered in firebase and in the dare db.
-  /// If an error occurs when attempting to register a user the future will contain an error
-  Future<FirebaseUser> handleRegistration(
-      String email, String userName, String password) async {
+  /// The method is atomic in nature. If an error occurs when attempting to
+  /// register a user. the user is not added to any db.
+  Future<FirebaseUser> handleRegistration(String email, String userName, String password) async {
     //First we create the user in firebase with the provided credentials.
-    //If the user is successfully created, we then create the user in the dare db with the uid of the firebase user.
-    //If adding the user to dare db fails for whatever reason the user is removed from firebase.
-    // This is because users that are signed up in firebase cannot re-register to be added in the dare db.
-    //Only registering in the user in firebase would exhaust that users email and result in a broken user.
+    //If the user is successfully created, we then create the user in the
+    // dare db with the uid of the firebase user.
+    //If adding the user to dare db fails for whatever reason the user is
+    // removed from firebase. This is because users that are signed up in
+    // firebase cannot re-register to be added in the dare db. Only
+    // registering in the user in firebase would exhaust that users email
+    // and result in a broken user.
+    //If the user could not be registered in either Firebase or Dare db a
+    // Platform exception is thrown
 
     //Create a user in firebase with the provided credentials, if this fails an exception is thrown
     final FirebaseUser user = (await _auth
@@ -52,14 +58,16 @@ class AuthLogic {
       String userAsJson =
           User(uid: user.user.uid, name: userName, email: email).toJson();
 
-      _authService.registerUser(userAsJson).catchError((error) {
+       return _authService.registerUser(userAsJson).then((_) {
+         return user;
+       }).catchError((error) {
         //Could not register the user in dare db, Remove the user from firebase
-        print("${error.toString()}");
+        print("[AuthLogic](_authService.registerUser): ${error.toString()}");
         user.user.delete();
+        throw PlatformException(code:"ERROR_DARE_SERVER", message:"The user could not be created in the Dare n Share server", details: null);
       });
-      return user;
-    }))
-        .user;
+
+    })).user;
 
     print("signed up user " + user.uid);
     return user;
